5aaeeeacd6437320ee251ad1658abf5d0484374b
Stevan Mladenovic <dzvrnjk@gmail.com>
2020-12-06 07:07:41 +0100

    day6 part two does not work

---
+++ solutions/day6.fsx
@@ -0,0 +1,22 @@
+﻿open System.IO
+
+let countYeses (question: string) =
+    question.Replace("\n", "").Trim()
+    |> Set.ofSeq
+    |> Set.count
+
+let countSameAnswers (question: string) =
+    question.Split([| "\n" |], System.StringSplitOptions.None)
+    |> Array.map (fun x -> x |> Set.ofSeq)
+    |> Set.intersectMany
+    |> Set.count
+
+let input =
+    (File.ReadAllText @"input\Day6\input.txt")
+        .Split([| "\n\n" |], System.StringSplitOptions.None)
+
+let solution =
+    input |> Array.map countYeses |> Array.sum
+
+let solution2 =
+    input |> Array.map countSameAnswers |> Array.sum
